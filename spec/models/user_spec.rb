3b054c22c17e021d3b8d8709a9af19ab2bf67f55
AlexRom <allrom67@gmail.com>
2019-08-16 00:02:40 +0300

    BDD#w/o_nested#3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,41 +12,28 @@ RSpec.describe User, type: :model do
   end
 
   describe '#author?' do
-    subject { user.author?(@object) }
-
     let(:owner) { User.new(id: 1, email: 'test1@exanple.edu') }
     let(:visitor) { User.new(id: 2, email: 'test2@exanple.edu') }
     let(:owners_question) { owner.questions.new(id: 1, title: 'TestTitle', body: 'TestBody') }
     let(:owners_answer) { owner.answers.new(id: 1, question_id: 1, body: 'Test') }
 
-    before do  |test|
-      if test.metadata[:question]
-        @object = owners_question
-      end
-      if test.metadata[:answer]
-        @object = owners_answer
-      end
-    end
-
     context 'if user is author'  do
-      let(:user) { owner }
-      it 'should return "true" for question', :question do
-        expect(subject).to eq(true)
+      it 'should return "true" for question' do
+        expect(owner).to be_author(owners_question)
       end
 
       it 'should return "true" for answer', :answer do
-        expect(subject).to eq(true)
+        expect(owner).to be_author(owners_answer)
       end
     end
 
     context 'if not author'  do
-      let(:user) { visitor }
-      it 'should return "false" for question', :question do
-        expect(subject).to eq(false)
+      it 'should return "false" for question' do
+        expect(visitor).not_to be_author(owners_question)
       end
 
       it 'should return "false" for answer', :answer do
-        expect(subject).to eq(false)
+        expect(visitor).not_to be_author(owners_answer)
       end
     end
   end
