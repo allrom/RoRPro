4d3819b893d48eec383d8e9707496de41badd9c2
AlexRom <allrom67@gmail.com>
2019-09-13 01:25:23 +0300

    NestedForms&Poly#4

--- spec/helpers/url_match_spec.rb
+++ spec/helpers/url_match_spec.rb
@@ -1,10 +1,5 @@
 require 'spec_helper'
-
-module LinksHelper
-  def gist_url?(url)
-    url =~ /^(?i)https:\/\/gist\.github\.com\/(?=.{1,39}$)(?![_\W])[a-z0-9-]+(?<![_\W])\/([a-z0-9\/.]+)$/
-  end
-end
+require 'links_helper'
 
 class TestClass
   include LinksHelper
