e2404d7c0f72eb5fbc471b454b7985426868b97f
AlexRom <allrom67@gmail.com>
2019-10-22 16:03:21 +0300

    OAuth#2

--- app/controllers/oauth_confirmations_controller.rb
+++ app/controllers/oauth_confirmations_controller.rb
@@ -13,11 +13,11 @@ class OauthConfirmationsController < Devise::ConfirmationsController
 
   protected
 
+  # overrides devise protected method to create authorization after if no email in providers response
   def after_confirmation_path_for(resource_name, user)
-    # overrides devise protected method to create authorization after if no email in providers response
     auth = { provider: session[:oauth_provider], uid: session[:oauth_uid] }
 
-    user.authorizations.create(auth) if auth.values.all?
+    user.authorizations.create!(auth) if auth.values.all?
     sign_in user, event: :authentication
     signed_in_root_path(user)
   end
